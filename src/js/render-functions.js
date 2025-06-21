1c13020f42af532e5914ec0559c968f6ca4ed7aa
your_name <pastushenkodmitry2003@gmail.com>
2025-06-22 00:42:39 +0300

    js hw 12 minimally working

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -4,42 +4,39 @@ import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів <-- Жахлива підказка
 // import 'simplelightbox/dist/simple-lightbox.min.css'; <-- I HATE this
 
+const slb = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  // captionDelay: 250,
+});
+
 const gallery = document.querySelector('ul.gallery');
-const loader = document.querySelector('span.loader');
 // createGallery(images). Ця функція повинна приймати масив images, створювати HTML-розмітку для галереї, додавати її в контейнер галереї та викликати метод екземпляра SimpleLightbox refresh(). Нічого не повертає.
 export function createGallery(images) {
-  clearGallery();
-  //
+  // clearGallery();
+  hideLoader();
   // console.log(gallery);
 
-  gallery.innerHTML = images
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        preview = webformatURL,
-        original = largeImageURL,
-        description = tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => `<li class="gallery-item">
-    <a class="gallery-link" href="${original}">
+  gallery.insertAdjacentHTML(
+    'beforeend',
+    images
+      .map(
+        ({
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => `<li class="gallery-item">
+    <a class="gallery-link" href="${largeImageURL}">
       <img
         class="gallery-image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
+        src="${webformatURL}"
+        data-source="${largeImageURL}"
+        alt="${tags}"
       />
     </a>
-      <!--<div class="image-info">
-      <span>likes: ${likes}</span>
-      <span>vievs: ${views}</span>
-      <span>comments: ${comments}</span>
-      <span">downloads: ${downloads}</span>
-      </div>-->
       <ul class="image-info">
         <li><h4>Likes</h4><p>${likes}</p></li>
         <li><h4>Views</h4><p>${views}</p></li>
@@ -47,48 +44,47 @@ export function createGallery(images) {
         <li><h4>Downloads</h4><p>${downloads}</p></li>
       </ul>
   </li>`
-    )
-    .join('');
-  // gallery.insertAdjacentHTML(
-  //   'afterbegin',
-  //   images
-  //     .map(
-  //       ({ preview, original, description }) => `<li class="gallery-item">
-  //   <a class="gallery-link" href="${original}">
-  //     <img
-  //       class="gallery-image"
-  //       src="${preview}"
-  //       data-source="${original}"
-  //       alt="${description}"
-  //     />
-  //     fuck
-  //     <span>1</span>
-  //     <span>2</span>
-  //     <span>3</span>
-  //     <span>4</span>
-  //   </a>
-  // </li>`
-  //     )
-  //     .join('')
-  // );
-  const slb = new SimpleLightbox('.gallery a', {});
+      )
+      .join('')
+  );
+
   slb.refresh();
 }
-// clearGallery(). Ця функція нічого не приймає та повинна очищати вміст контейнера галереї. Нічого не повертає.
-export function clearGallery() {
-  gallery.innerHTML = '';
-  //
-}
+// gallery.insertAdjacentHTML(
+//   'afterbegin',
+//   images
+//     .map(
+//       ({ preview, original, description }) => `<li class="gallery-item">
+//   <a class="gallery-link" href="${original}">
+//     <img
+//       class="gallery-image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//     fuck
+//     <span>1</span>
+//     <span>2</span>
+//     <span>3</span>
+//     <span>4</span>
+//   </a>
+// </li>`
+//     )
+//     .join('')
+// );
 
+// clearGallery(). Ця функція нічого не приймає та повинна очищати вміст контейнера галереї. Нічого не повертає.
+export const clearGallery = () => (gallery.innerHTML = '');
+
+const loader = document.querySelector('span.loader');
 // showLoader(). Ця функція нічого не приймає, повинна додавати клас для відображення лоадера. Нічого не повертає.
-export function showLoader() {
-  // gallery.classList.add('.loading');
-  // loader.classList.add('.loader-show');
-  loader.classList.remove('hidden');
-}
+export const showLoader = () => loader.classList.remove('hidden');
 // hideLoader(). Ця функція нічого не приймає, повинна прибирати клас для відображення лоадера. Нічого не повертає.
-export function hideLoader() {
-  // gallery.classList.remove('.loading');
-  // loader.classList.remove('.loader-show');
-  loader.classList.add('hidden');
-}
+export const hideLoader = () => loader.classList.add('hidden');
+
+const loadMore = document.querySelector('button.load-more');
+// showLoadMoreButton(). Ця функція нічого не приймає, повинна додавати клас для відображення кнопки Load more. Нічого не повертає.
+export const showLoadMoreButton = () => loadMore.classList.remove('hidden');
+
+// hideLoadMoreButton(). Ця функція нічого не приймає, повинна прибирати клас для відображення кнопки Load more. Нічого не повертає.
+export const hideLoadMoreButton = () => loadMore.classList.add('hidden');
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 // У файлі main.js напиши всю логіку роботи додатка. Виклики нотифікацій iziToast, усі перевірки на довжину масиву в отриманій відповіді робимо саме в цьому файлі. Імпортуй в нього функції із файлів pixabay-api.js та render-functions.js та викликай їх у відповідний момент.
+// логіку прокручування сторінки (scroll) робимо саме в цьому файлі. Імпортуй в нього функції із файлів pixabay-api.js та render-functions.js та викликай їх у відповідний момент.
 import fetchData from './js/pixabay-api';
 import * as render from './js/render-functions';
 // Описаний у документації
@@ -7,34 +8,70 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('form.form');
-form.addEventListener('submit', search => {
+
+let page = 1;
+let searchQuery = '';
+form.addEventListener('submit', async search => {
   search.preventDefault();
+  page = 1;
   // console.log(search.target.elements[0].value);
-  if (search.target.elements[0].value) {
+  if (search.target.elements[0].value.trim()) {
+    searchQuery = search.target.elements[0].value.trim();
     render.clearGallery();
     render.showLoader();
-    fetchData(search.target.elements[0].value)
-      .then(res => {
-        // console.log(res);
-        render.hideLoader();
-        render.createGallery(res.data.hits);
-        if (!res.data.total)
-          throw new Error(
-            'Sorry, there are no images matching your search query. Please try again!'
-          );
-      })
-      .catch(err => {
-        // console.log(err);
-        iziToast.error({
-          // title: 'Error',
-          message: String(err),
+    try {
+      const res = await fetchData(searchQuery, page++),
+        {
+          data: { hits, total, totalHits },
+        } = res;
+
+      // const {
+      //   res,
+      //   res: {
+      //     data: { hits, total, totalHits },
+      //   },
+      // } = { res: await fetchData(search.target.elements[0].value) };
+
+      // console.log(res);
+      if (Math.ceil(total / 15) >= page) render.showLoadMoreButton();
+      else
+        iziToast.info({
+          message: "We're sorry, but you've reached the end of search results.",
           position: 'topRight',
         });
+
+      render.createGallery(res.data.hits);
+      if (!res.data.total)
+        throw new Error(
+          'Sorry, there are no images matching your search query. Please try again!'
+        );
+    } catch (err) {
+      console.log(err);
+      // console.log(res, res.data.hits);
+      iziToast.error({
+        message: String(err),
+        position: 'topRight',
       });
-    // iziToast.success({
-    //   message: 'sus',
-    //   position: 'topRight',
-    // });
+    }
+    render.hideLoader();
+    // fetchData(search.target.elements[0].value)
+    //   .then(res => {
+    //     // console.log(res);
+    //     render.hideLoader();
+    //     render.createGallery(res.data.hits);
+    //     if (!res.data.total)
+    //       throw new Error(
+    //         'Sorry, there are no images matching your search query. Please try again!'
+    //       );
+    //   })
+    //   .catch(err => {
+    //     // console.log(err);
+    //     iziToast.error({
+    //       // title: 'Error',
+    //       message: String(err),
+    //       position: 'topRight',
+    //     });
+    //   });
   } else
     iziToast.error({
       message: 'red',
@@ -42,70 +79,44 @@ form.addEventListener('submit', search => {
     });
 });
 
-// const images = [
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-//     description: 'Hokkaido Flower',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-//     description: 'Container Haulage Freight',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-//     description: 'Aerial Beach View',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-//     description: 'Flower Blooms',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-//     description: 'Alpine Mountains',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-//     description: 'Mountain Lake Sailing',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-//     description: 'Alpine Spring Meadows',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-//     description: 'Nature Landscape',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-//     description: 'Lighthouse Coast Sea',
-//   },
-// ];
+const loadMoreBtn = document.querySelector('button.load-more');
+loadMoreBtn.addEventListener('click', async () => {
+  // console.log(loadMoreBtn);
+  render.hideLoadMoreButton();
+  render.showLoader();
 
-// render.createGallery(images);
+  if (searchQuery) {
+    try {
+      const res = await fetchData(searchQuery, page++),
+        {
+          data: { hits, total, totalHits },
+        } = res;
+
+      // console.log(res);
+      if (Math.ceil(total / 15) >= page) render.showLoadMoreButton();
+      else
+        iziToast.info({
+          message: "We're sorry, but you've reached the end of search results.",
+          position: 'topRight',
+        });
+
+      render.createGallery(res.data.hits);
+      if (!res.data.total)
+        throw new Error(
+          'Sorry, there are no images matching your search query. Please try again!'
+        );
+    } catch (err) {
+      console.log(err);
+      // console.log(res, res.data.hits);
+      iziToast.error({
+        message: String(err),
+        position: 'topRight',
+      });
+    }
+  }
+  render.hideLoader();
+  // render.showLoadMoreButton();
+
+  const galleryItem = document.querySelector('li.gallery-item');
+  window.scrollBy(0, galleryItem.getBoundingClientRect().height * 2);
+});
