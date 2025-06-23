4057a67a5462909571ad851dabb278e130669d9c
your_name <pastushenkodmitry2003@gmail.com>
2025-06-23 17:27:50 +0300

    Redo 1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 // У файлі pixabay-api.js зберігай функції для виконання HTTP-запитів:
 
 // getImagesByQuery(query). Ця функція повинна приймати один параметр query (пошукове слово, яке є рядком), здійснювати HTTP-запит і повертати значення властивості data з отриманої відповіді.
-export default function getImagesByQuery(query, page = 1) {
-  return axios.get('https://pixabay.com/api/', {
+export default async function getImagesByQuery(query, page = 1) {
+  return await axios.get('https://pixabay.com/api/', {
     params: {
       key: '50866310-f6e992bbe1b9ca34c705df120',
       q: query,
